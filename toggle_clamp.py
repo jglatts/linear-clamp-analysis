d76b1b28c20aa2730f600c78794d67e67bf31eb1
John D. Glatts <John@z-axiscc.com>
2022-11-14 14:07:06 -0500

    add animation

--- toggle_clamp.py
+++ toggle_clamp.py
@@ -2,6 +2,8 @@
 # Inspired by https://www.instructables.com/The-Linear-Toggle-Clamp/
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib import rc
 
 def calcPts(theta, lhandle, lpivot, lbar):
     t = theta * np.pi/180
@@ -56,6 +58,43 @@ def mechAdvantage(lhandle, lpivot, lbar, theta):
     axis[1].plot(tt[0:89], mechanical_advantage,'-')
     plt.show()
 
+def calcClampForce():
+    pass
+
+def sysAnimated(frameNumber):  
+    ax.clear()
+    theta_min = -90
+    theta_max = 90
+    frameMax = 50
+    dtheta =  float(theta_max - theta_min) / float(frameMax-1)
+    theta = frameNumber * dtheta - 90
+
+    # set up the lengths of each piece
+    lhandle = 20
+    lpivot = 15
+    lbar = 13
+    lplunger = 12
+    xbase_min = -13
+    xbase_max = 87
+
+    # calculate the key pionts
+    xpivot, ypivot, xhandle, yhandle, xpiston = calcPts(theta, lhandle, lpivot, lbar)
+
+    # set up the graph
+    plt.title("diagram of the clamp")
+    ax.set_xlim(-80,180)
+    ax.set_ylim(-20,110)
+    ax.set_aspect(1)
+
+    # plot the points
+    ax.plot( [0,xpivot, xhandle], [0, ypivot, yhandle],"*-", linewidth=3, label="lever")
+    ax.plot([xpivot, xpiston], [ypivot, 0],'*-',linewidth=3, label="arm")
+    ax.plot([xpiston-lplunger/2, xpiston+lplunger/2],[0,0], linewidth=3, label="plunger")
+    ax.plot([xbase_min, xbase_max], [-5,-5], linewidth=3, label="base")
+    
+    ax.legend()
+    return ax.plot
+
 
 def computePoints():
     # compute points of toggle clamp
@@ -67,7 +106,15 @@ def computePoints():
     plt.plot([xpivot, xpiston], [ypivot, 0],'*-')
     plt.show()
 
+def makeAnimation():
+    global fig
+    global ax
+    fig = plt.figure(figsize=(8,6))
+    ax = plt.axes()
+    anim = animation.FuncAnimation(fig, sysAnimated, frames=50, blit=False, repeat=True)
+    plt.show()
+
 
 if __name__ == '__main__':
-    mechAdvantage(20.32, 15, 13.43, 69)
-
+    #mechAdvantage(20.32, 15, 13.43, 69)
+    makeAnimation()
